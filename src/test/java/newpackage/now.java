f23a07efb289dc677589018ca59a292358992a20
pradeep <pradeep7725@gmail.com>
2023-07-28 12:24:24 +0530

    correct merge

--- src/test/java/newpackage/now.java
+++ src/test/java/newpackage/now.java
@@ -3,14 +3,10 @@ package newpackage;
 public class now {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		
-      System.out.println("pradeep kumara");
-=======
+
 		System.out.println("hii");
 		System.out.println("kumarapradeep");
 
->>>>>>> blr
 	}
 
 }
