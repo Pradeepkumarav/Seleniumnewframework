89ea8936b4d3009455723934487ffbf8353f6727
pradeep <pradeep7725@gmail.com>
2023-07-30 17:46:14 +0530

    utility base url 300

--- src/test/java/newpackage/utility.java
+++ src/test/java/newpackage/utility.java
@@ -1,9 +1,10 @@
 package newpackage;
 
 public class utility {
-
+     Baseurl:https:flipkart.com
 	public static void main(String[] args) {
-		System.out.println("utility");
+		
+System.out.println("utility");
 
 	}
 
